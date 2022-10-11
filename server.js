8593f4b2020ceedd77b3413afada16614074ed36
Federico Fracaro <federicohfracaro@gmail.com>
2022-10-11 18:14:15 -0300

    desafio6

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const {Server:HttpServer}=require('http');
+const {Server:IOServer}=require('socket.io');
+const httpServer=new HttpServer(app);
+const io=new IOServer(httpServer);
+// NOTA : OTRA FORMA DE LLAMARLOS
+// const server = require('http').Server(app)
+// const io = require('socket.io')(server)
+let messages = [];
+let products = [];
+
+app.use(express.static('public'));
+
+io.on('connection',socket=> {
+    console.log('Un cliente se ha conectado');
+    socket.emit('messages', messages);
+    socket.emit('products',products);
+    socket.on('new-message',data=> {
+        messages.push(data); 
+        io.sockets.emit('messages', messages); 
+    });
+    socket.on('new-product',data2=> {
+        products.push(data2);
+        io.sockets.emit('products', products);
+    })    
+});
+
+
+const PORT = process.env.PORT || 8080;
+
+const server = httpServer.listen(PORT, () => { 
+    console.log(`Servidor Http con Websockets escuchando en el puerto ${server.address().port}`);
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
